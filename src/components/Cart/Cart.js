a8253e7394fd54cd7eada2f635dd71c5002c4606
Tyler Whidden <Ty.whidden@gmail.com>
2021-06-07 15:51:00 -0400

    working on adding firebase. ProductItem still has some bugs to be fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,7 +10,7 @@ const Cart = (props) => {
       <h2>Your Shopping Cart</h2>
       <ul>
       {cartItems.map((item) => (
-        <CartItem  item={{title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price }} />
+        <CartItem key={item.id}  item={{id: item.id, title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price }} />
       ))}
       </ul>
     </Card>
